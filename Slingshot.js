f67196740f0664ee79bae1a1a07cd89e02656df3
msdarshan2006 <72445666+msdarshan2006@users.noreply.github.com>
2021-01-20 17:00:17 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,27 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        // for the bird to hit the pig
+        if(this.sling.bodyA!=null){
+              var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+      
+    }
+    birdfly(){
+        this.sling.bodyA = null
+    }
+    
+}
